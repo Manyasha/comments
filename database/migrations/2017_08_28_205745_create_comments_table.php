7747a88a661314da274bf704972f44530a39fa45
Manyasha <univers94@ukr.net>
2017-08-29 00:07:07 +0300

    create db

---
+++ database/migrations/2017_08_28_205745_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('comments')) { return; }
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamp('parent_created_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
